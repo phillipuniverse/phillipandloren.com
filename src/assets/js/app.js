614cf1684eaacb51c0b2e9459ce5d3923209bf23
Phillip Verheyden <pverheyden@gmail.com>
2016-04-26 00:29:32 -0500

    Update wedding party images and subsequent js to swap them out, fix some of the google maps js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,24 +2,42 @@ $(document).foundation();
 
 $('img.thumbnail').hover(function() {
   var src = this.src;
+  var sillyext = $(this).data('sillyext');
+  var normalext = $(this).data('normalext');
+
   if (src.indexOf('_silly') == -1) {
     // Attach the silly in-between the image URL and the file extension
     // e.g. /img/tay.jpg -> /img/tay_silly.jpg
-    this.src = src.substr(0, src.lastIndexOf('.')) + '_silly' + src.substr(src.lastIndexOf('.'));
+
+    this.src = src.substr(0, src.lastIndexOf('.')) + '_silly';
+    if (sillyext) {
+      this.src += '.' + sillyext;
+      return;
+    } else {
+      this.src += src.substr(src.lastIndexOf('.'));
+    }
   } else {
     // remove the _silly
     this.src = src.replace('_silly', '');
+    if (normalext) {
+      this.src = this.src.substr(0, this.src.lastIndexOf('.')) + '.' + normalext
+    }
   }
 });
 
 var map;
 var center;
+var bounds;
 function initMap() {
   center = {lat: 30.29572797626866, lng: -97.8402841479492};
   map = new google.maps.Map(document.getElementById('map_container'), {
           center: center,
-          zoom: 10
+          zoom: 12
         });
+
+  google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
+    bounds = map.getBounds();
+  });
 
   var starhillMarker = new google.maps.Marker({
     position: {lat: 30.306641, lng: -97.981168},
@@ -46,5 +64,6 @@ function initMap() {
     var center = map.getCenter();
     google.maps.event.trigger(map, 'resize')
     map.setCenter(center);
+    map.fitBounds(bounds);
   })
 }
